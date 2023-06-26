c18e916a92fcdf740c7de954f661b36a9e9aea03
RickyRacz <134242969+RickyRacz@users.noreply.github.com>
2023-06-25 23:03:49 -0400

    First commit
    got the hidden file error many times. Please just get all the files from the .zip file uploaded in ecentennial.

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://RickyR:comp229midterm@booklistcluster.a86toau.mongodb.net/?retryWrites=true&w=majority"
+  //"mongodb://localhost/books229"
+};
